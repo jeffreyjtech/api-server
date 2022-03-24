8c259e13dff5db55de07d656f69a68315ea78bae
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-24 16:37:04 -0700

    Added basic sequelize code to models/index

---
+++ src/models/index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const { Sequelize, DataTypes } = require('sequelize');
+const magikarpSchema = require('./magikarp.schema');
+
+const CollectionClass = require('./collection-class');
+
+const DATABASE_URL = process.env.NODE_ENV === 'test' ?
+  'sqlite:memory' :
+  process.env.DATABASE_URL || 'postgresql://localhost:5432';
+
+const sequelize = new Sequelize(DATABASE_URL,
+  { // Heroku stuff from class repo "class-03"
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });  
+
+const MagikarpModel = magikarpSchema(sequelize, DataTypes);
+
+module.exports = {
+  sequelize,
+  MagikarpCollection: new CollectionClass(MagikarpModel),
+};
