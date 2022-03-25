c631b9f9b12f4e36ef5d31757e8f1edc40e1f2cc
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-24 21:59:13 -0700

    Added bad HTTP method test

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -6,8 +6,14 @@ const request = supertest(server.app);
 
 describe('Testing that API returns error status on bad requests', () => {
 
-  test('Test for 404 on bad route', async () => {
-    const response = request.get('/potato');
+  test('Test for 404 on a bad route', async () => {
+    const response = await request.get('/potato');
+
+    expect(response.status).toEqual(404);
+  });
+
+  test('Test for 404 on a bad method', async () => {
+    const response = await request.patch('/magikarp');
 
     expect(response.status).toEqual(404);
   });
