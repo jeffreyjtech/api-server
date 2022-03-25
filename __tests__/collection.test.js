92d898eb43700f6c5f7ee98c18c93cc5c13f2056
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-24 21:12:07 -0700

    Added "get all" test to collection tests

--- __tests__/collection.test.js
+++ __tests__/collection.test.js
@@ -12,12 +12,34 @@ afterAll(async () => {
 });
 
 describe('Testing collections', () => {
+  let name = 'Billy Bob';
+  let shiny = true;
+  
   test('Should create a Magikarp', async () => {
-    let name = 'Billy Bob';
-    let shiny = true;
 
     const magikarp = await MagikarpCollection.create({ name, shiny });
 
     expect(magikarp.name).toEqual(name);
+  });
+
+  test('Should throw an unhandled error if passing in invalid data', async () => {
+    let name = 'Billy Bob';
+
+    expect.assertions(1);
+
+    try {
+      await MagikarpCollection.create({ name });
+    } catch (err) {
+      expect(err).toBeInstanceOf(Error);
+    }
+  });
+
+  test('Should get all Magikarps', async () => {
+
+    const magikarpArray = await MagikarpCollection.readAll();
+
+    console.log(magikarpArray);
+    
+    expect(magikarpArray[0].name).toEqual(name);
   });
 });
